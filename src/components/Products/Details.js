f85444674237fe3d715b80cc3b62043174945a98
hugotata78 <hugotatarinoff@gmail.com>
2021-09-14 10:00:50 -0300

    mejoras componente details

--- src/components/Products/Details.js
+++ src/components/Products/Details.js
@@ -1,12 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { DataContext } from "../../Context/DataProvider";
+import CardProduct from "./CardProduct";
 
 
 const Details = () => {
 
     const value = useContext(DataContext)
     const [products] = value.products
+    const add = value.add
     const { id } = useParams()
     const [details, setDetails] = useState([])
     const [url, setUrl] = useState(0)
@@ -22,9 +24,12 @@ const Details = () => {
     }, [setDetails, id, products])
 
     useEffect(() => {
-        const values = `${details.img1}${url}${details.img2}`
+        const img1 = details && details.img1
+        const img2 = details && details.img2
+        const values = `${img1}${url}${img2}`
         setImages(values)
-    }, [])
+
+    }, [details, url])
 
     const handleInputChange = (e) => {
         const number = e.target.value.toString().padStart(2, '01')
@@ -32,7 +37,8 @@ const Details = () => {
         console.log(url)
     }
 
-    console.log(images)
+    if (details.length < 1) return null;
+
     return (
         <div className='details'>
             <h2>{details.title}</h2>
@@ -54,16 +60,30 @@ const Details = () => {
                     <p>Size</p>
                 </div>
             </div>
-            <button>Add to Cart</button>
+            <button onClick={()=>add(details.id)}>Add to Cart</button>
             {
-                url ?
+                url !== 0 ?
                     <img src={images} alt='img' />
                     :
-                    <img src={details.image && details.image.default} alt='img' />
+                    <img src={details.image.default} alt='img' />
             }
             <input type='range' min='1' max='36' value={url} onChange={handleInputChange} />
             <div className='description'>
                 <p><b>Description: </b>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <br/>
+            <h2 className='related'>related products</h2>
+            <div className='products'>
+                {
+                    products.map((product,index)=>{
+                        if((index < 6 )&&(product.category === details.category)){
+                            return(
+                                <CardProduct product={product} key={product.id} />
+                            )
+                        }
+                        
+                    })
+                }
             </div>
         </div>
     )
